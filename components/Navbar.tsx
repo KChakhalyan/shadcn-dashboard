58830c9d086b2429729e1cceef2f657051bb8778
KChakhalyan <karenchakhalyan23@gmail.com>
2025-08-14 21:10:53 +0400

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,71 @@
+import Link from "next/link"
+import { BadgeCheck, CreditCard, LogOut, Moon, Settings, SquareMenu, User, Users } from "lucide-react"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { Button } from "./ui/button"
+import { ModeToggle } from "./ModeToggle"
+
+
+const Navbar = () => {
+    return (
+        <nav className="flex p-4 items-center justify-between">
+            {/* LEFT */}
+            colapseButton
+            {/* RIGHT */}
+            <div className="flex items-center gap-4">
+                <Link href="/">Dashboard</Link>
+                <ModeToggle />
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://avatars.githubusercontent.com/u/10487372?v=4" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent sideOffset={10}>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            <User />
+                            Profile</DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <CreditCard />Billing
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Users />
+                            Team
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Settings />
+                            Settings
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <BadgeCheck />
+                            Subscription
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem variant="destructive">
+                            <LogOut />
+                            Log Out
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                {/* Screen Readers Only */}
+                {/* <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <SquareMenu />
+                            <span className="sr-only">Open Menu</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem>Item 1</DropdownMenuItem>
+                        <DropdownMenuItem>Item 2</DropdownMenuItem>
+                        <DropdownMenuItem>Item 3</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu> */}
+            </div>
+        </nav>
+    )
+}
+export default Navbar
