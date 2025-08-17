52067ff703e5357d59584de7ba3f23f5a4fb20a6
KChakhalyan <karenchakhalyan23@gmail.com>
2025-08-17 14:26:31 +0400

    finished CardList Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
+import CardList from "@/components/CardList";
 import Barchart from "@/components/charts/Barchart";
 import ChartAreaLegend from "@/components/charts/ChartAreaLegend";
 import PieChartDonut from "@/components/charts/PieChartDonut";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-2 2xl:col-span-2"><Barchart /></div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><Barchart /></div>
+      <div className="bg-primary-foreground p-4 rounded-lg "><CardList title="Latest Transactions" /></div>
       <div className="bg-primary-foreground p-4 rounded-lg "><PieChartDonut /></div>
       <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><ChartAreaLegend /></div>
-      <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg "><CardList title="Popular Content" /></div>
     </div>
   );
 }
--- components/charts/Barchart.tsx
+++ components/charts/Barchart.tsx
@@ -1,6 +1,33 @@
-'use client'
-import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
-import { type ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A multiple bar chart"
+
+const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+]
 
 const chartConfig = {
     desktop: {
@@ -13,41 +40,41 @@ const chartConfig = {
     },
 } satisfies ChartConfig
 
-const chartData = [
-    { month: "January", desktop: 186, mobile: 80 },
-    { month: "February", desktop: 305, mobile: 200 },
-    { month: "March", desktop: 237, mobile: 120 },
-    { month: "April", desktop: 73, mobile: 190 },
-    { month: "May", desktop: 209, mobile: 130 },
-    { month: "June", desktop: 214, mobile: 140 },
-]
-
-const Barchart = () => {
-    return (<>
-        <h1 className="text-lg font-medium mb-6">Total Revenue</h1>
-        <ChartContainer config={chartConfig} className="h-[200px] w-full">
-            <BarChart accessibilityLayer data={chartData}>
-                <CartesianGrid vertical={false} />
-                <XAxis
-                    dataKey="month"
-                    tickLine={false}
-                    tickMargin={10}
-                    axisLine={false}
-                    tickFormatter={(value) => value.slice(0, 3)}
-                />
-                <YAxis
-                    tickLine={false}
-                    tickMargin={10}
-                    axisLine={false}
-                />
-                <ChartTooltip content={<ChartTooltipContent />} />
-                <ChartLegend content={<ChartLegendContent />} />
-
-                <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-                <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-            </BarChart>
-        </ChartContainer>
-    </>
+export default function ChartBarMultiple() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Bar Chart - Multiple</CardTitle>
+                <CardDescription>January - June 2024</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig}>
+                    <BarChart accessibilityLayer data={chartData}>
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="month"
+                            tickLine={false}
+                            tickMargin={10}
+                            axisLine={false}
+                            tickFormatter={(value) => value.slice(0, 3)}
+                        />
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent indicator="dashed" />}
+                        />
+                        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+                        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+                    </BarChart>
+                </ChartContainer>
+            </CardContent>
+            <CardFooter className="flex-col items-start gap-2 text-sm">
+                <div className="flex gap-2 leading-none font-medium">
+                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+                </div>
+                <div className="text-muted-foreground leading-none">
+                    Showing total visitors for the last 6 months
+                </div>
+            </CardFooter>
+        </Card>
     )
 }
-export default Barchart
