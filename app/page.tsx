1f18d8ae86fba65d6f24b2306819ec80594de673
KChakhalyan <karenchakhalyan23@gmail.com>
2025-08-18 10:03:44 +0400

    some sizing fizes on layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,28 @@ import TodoList from "@/components/TodoList";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><Barchart /></div>
-      <div className="bg-primary-foreground p-4 rounded-lg "><CardList title="Latest Transactions" /></div>
-      <div className="bg-primary-foreground p-4 rounded-lg "><PieChartDonut /></div>
-      <div className="bg-primary-foreground p-4 rounded-lg "><TodoList /></div>
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><ChartAreaLegend /></div>
-      <div className="bg-primary-foreground p-4 rounded-lg "><CardList title="Popular Content" /></div>
+    <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4  rounded-lg lg:col-span-2 xl:col-span-2 2xl:col-span-2">
+        <Barchart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <CardList title="Latest Transactions" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <PieChartDonut />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-2 2xl:col-span-2">
+        <ChartAreaLegend />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <TodoList />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <CardList title="Popular Content" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <CardList title="Recent Content" />
+      </div>
     </div>
   );
 }
